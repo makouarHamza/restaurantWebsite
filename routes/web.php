7d1ac56d89d751255ccc857a050fb4d842e2dfab
ghareeb <hamzamekoire@gmail.com>
2026-03-07 02:09:40 +0000

    Remove Breeze scaffolding and install Jetstream scaffolding | profile managemt

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
-
-require __DIR__.'/auth.php';
