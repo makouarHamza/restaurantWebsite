0c82ca3841c11f8cbbab926837bd77848ea3c92a
ghareeb <hamzamekoire@gmail.com>
2026-03-07 12:35:07 +0000

    "separate dashboard for the admin and user using Usercontroller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function home(){
+        if(Auth::id() && Auth::user()->role === "admin"){
+            return view("admin.dashboard");
+
+        }
+        else if(Auth::id() && Auth::user()->role === 'user'){
+            return view('dashboard');
+
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,7 +13,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [UserController::class, 'home'])->name('dashboard');
 });
