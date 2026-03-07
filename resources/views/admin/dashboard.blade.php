3d18baa4f512ccbbfb239ed7d9439b2a7d7b1ef0
ghareeb <hamzamekoire@gmail.com>
2026-03-07 17:00:13 +0000

    add a desgin and structure of admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __(' admin Dashboard') }}
@@ -6,4 +6,10 @@
     </x-slot>
 
 
-</x-app-layout>
+</x-app-layout> --}}
+
+@extends('admin.maindesgin');
+@section('dashboardAdmin')
+<h2>Admin Dashboard</h2>
+
+@endsection
